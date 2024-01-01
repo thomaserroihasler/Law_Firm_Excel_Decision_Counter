6fa76062bc0ba7755c44999afc56ca9f16f7098f
thomaserroihasler <hakainokokoro@gmail.com>
2024-01-01 11:45:51 +0400

    Finalized Project
    Finalized Project

--- main.py
+++ main.py
@@ -1,72 +1,28 @@
 import pandas as pd
+from collections import defaultdict
 from utils import get_executable_path
 from data_processing import extract_decisions_and_cases
-from excel_operations import remove_inner_borders, fill_cells, set_outer_border, write_in_cells, cell_alignment, auto_adjust_column_width
-from collections import defaultdict
-
-import xlsxwriter
-
-def plot_frequency_histogram(workbook, worksheet_name, data, chart_title, start_cell='A1'):
-    """
-    Plots a frequency histogram in an Excel worksheet using xlsxwriter.
-
-    Args:
-        workbook (xlsxwriter.Workbook): An xlsxwriter Workbook object.
-        worksheet_name (str): Name of the worksheet where the histogram will be plotted.
-        data (list): A list of data points for the histogram.
-        chart_title (str): Title of the histogram chart.
-        start_cell (str, optional): The starting cell for plotting data. Defaults to 'A1'.
-    """
-    # Create a worksheet
-    worksheet = workbook.add_worksheet(worksheet_name)
-
-    # Write the data to the worksheet
-    row = start_cell[1:]
-    col = start_cell[0]
-    for i, value in enumerate(data):
-        worksheet.write(f'{col}{int(row) + i}', value)
-
-    # Create a chart object
-    chart = workbook.add_chart({'type': 'column'})
-
-    # Configure the chart
-    chart.add_series({
-        'categories': f'={worksheet_name}!${col}${row}:${col}${int(row) + len(data) - 1}',
-        'values': f'={worksheet_name}!${col}${row}:${col}${int(row) + len(data) - 1}',
-        'gap': 2,
-    })
-
-    # Add a chart title
-    chart.set_title({'name': chart_title})
-
-    # Insert the chart into the worksheet
-    worksheet.insert_chart(f'{col}{int(row) + len(data) + 2}', chart)
-
-# Example usage
-workbook = xlsxwriter.Workbook('histogram_example.xlsx')
-data = [4, 5, 6, 7, 8, 9, 10, 11, 12]  # Example data
-plot_frequency_histogram(workbook, 'Histogram', data, 'Sample Frequency Histogram')
-workbook.close()
-
-
+from excel_operations import remove_inner_borders, fill_cells, set_outer_border
+from excel_operations import write_in_cells, cell_alignment, auto_adjust_column_width
+from excel_operations import     plot_frequency_bar_chart
+from collections import Counter
 
 def main():
     print("Current Working Directory:", get_executable_path())
 
-
     input_file_path = input("Please enter the name of the excel file (without extension): ")
     output_file_path = input("Please enter the name of the saved excel file (without extension): ")
 
+    input_file_path += '.xlsx'
+    output_file_path += '.xlsx'
 
-    input_file_path = input_file_path + '.xlsx'
-    output_file_path = output_file_path + '.xlsx'
     try:
         df = pd.read_excel(get_executable_path() + '/' + input_file_path)
     except Exception as e:
         print(f"An error occurred while reading the Excel file: {e}")
         return
 
-    df = df.iloc[:, [0, 2]]  # Selecting specific columns; adjust as needed
+    df = df.iloc[:, [0, 2]]  # Selecting specific columns
     df.iloc[:, 1] = pd.to_datetime(df.iloc[:, 1]).dt.date  # Convert to date-only format
 
     decision_case_counts = defaultdict(int)
@@ -77,14 +33,14 @@ def main():
         decision_date = row.iloc[1]
         cases = extract_decisions_and_cases(decision_name)
         cases_string = ' & '.join(cases)
-        decisions.append([None, decision_name, decision_date, cases_string])  # Add None for the new first column
+        decisions.append([None, decision_name, decision_date, cases_string])
         decision_case_counts[(cases_string, decision_date)] += 1
 
     all_decisions_df = pd.DataFrame(decisions, columns=[None, 'Filename', 'Decision_Date', 'Cases_Numbers'])
     all_decisions_df = all_decisions_df.sort_values(by='Decision_Date', ascending=False)
 
-    # Write to Excel
-    with pd.ExcelWriter(get_executable_path() + '/' + output_file_path, engine='openpyxl') as writer:
+    output_path = get_executable_path() + '/' + output_file_path
+    with pd.ExcelWriter(output_path, engine='openpyxl') as writer:
         all_decisions_df.to_excel(writer, index=False, sheet_name='Cases_Numbers')
 
         workbook = writer.book
@@ -98,25 +54,69 @@ def main():
             decision_count = decision_case_counts[(row['Cases_Numbers'], row['Decision_Date'])]
             end_row = row_idx + decision_count - 1
 
-            cell_range = f'B{row_idx}:D{end_row}'  # Adjust cell range
-            cell_range_counter = f'A{row_idx}:A{end_row}'  # Adjust cell range
+            cell_range = f'B{row_idx}:D{end_row}'
+            cell_range_counter = f'A{row_idx}:A{end_row}'
             remove_inner_borders(worksheet, cell_range)
             if end_row != row_idx:
                 fill_cells(worksheet, cell_range, 'solid', '00FF00')
                 set_outer_border(worksheet, cell_range, 'thick')
-
 
             write_in_cells(worksheet, cell_range_counter, str(counter))
             set_outer_border(worksheet, cell_range_counter, 'thick')
 
             row_idx = end_row + 1
             counter += 1
-        cell_alignment(worksheet)
+
+        cell_alignment(worksheet,'A')
+        cell_alignment(worksheet, 'C')
+        cell_alignment(worksheet, 'D')
+
         auto_adjust_column_width(worksheet)
         set_outer_border(worksheet, f'B2:B{row_idx-1}', 'thin')
         set_outer_border(worksheet, f'C2:C{row_idx-1}', 'thin')
         set_outer_border(worksheet, None, 'thick')
         set_outer_border(worksheet, f'A1:D1', 'thick')
+        # Extract year from each datetime object
+        decision_years = [date.year for date in all_decisions_df['Decision_Date'] if pd.notnull(date)]
+        year_counts = Counter(decision_years)
+        data_for_chart = list(year_counts.items())
+        plot_frequency_bar_chart(workbook, 'Cases_Numbers', data_for_chart, 'Decision Date Frequency', 'F1')
+
+        # Define subsets
+        subset_1 = all_decisions_df[all_decisions_df['Filename'].str.contains('\[CAS Web Archives\]')]
+        subset_2 = all_decisions_df[all_decisions_df['Filename'].str.contains('CAS Bull')]
+        subset_3 = all_decisions_df[~all_decisions_df['Filename'].isin(subset_1['Filename']) & ~all_decisions_df['Filename'].isin(subset_2['Filename'])]
+
+        # Write subsets to different sheets
+        subsets = [subset_1,subset_2,subset_3]
+        sheet_names = ['CAS Web Archives','CAS Bull','Other']
+        for i, subset in enumerate(subsets, start=1):
+            sheet_name = sheet_names[i-1]
+            workbook.create_sheet(sheet_name)
+            subset.to_excel(writer, index=False, sheet_name=sheet_name)
+
+            subset_worksheet = writer.sheets[sheet_name]
+            # Apply formatting and add counter to the first column
+            row_idx = 2  # Start from the second row
+            for _, row in subset.iterrows():
+                write_in_cells(subset_worksheet, f'A{row_idx}', str(row_idx - 1))
+                row_idx += 1
+            cell_alignment(subset_worksheet, 'A')
+            cell_alignment(subset_worksheet, 'C')
+            cell_alignment(subset_worksheet, 'D')
+            auto_adjust_column_width(subset_worksheet)
+            set_outer_border(subset_worksheet, f'B2:B{row_idx - 1}', 'thin')
+            set_outer_border(subset_worksheet, f'C2:C{row_idx - 1}', 'thin')
+            set_outer_border(subset_worksheet, f'A1:D1', 'thick')
+            set_outer_border(subset_worksheet, None, 'thick')
+
+            # Plot frequency bar chart of decision dates
+            if 'Decision_Date' in subset:
+                # Extract year from each datetime object
+                decision_years = [date.year for date in subset['Decision_Date'] if pd.notnull(date)]
+                year_counts = Counter(decision_years)
+                data_for_chart = list(year_counts.items())
+                plot_frequency_bar_chart(workbook, sheet_name, data_for_chart, 'Decision Date Frequency','F1')
 
 if __name__ == "__main__":
     main()
--- main_total.py
+++ main_total.py
@@ -1,8 +1,65 @@
+import pandas as pd
+from collections import defaultdict
+from collections import Counter
+
 from openpyxl.styles import PatternFill, Border, Side, Alignment, Font
 from openpyxl.utils import range_boundaries
 from openpyxl.utils import get_column_letter
 from openpyxl.chart import BarChart, Reference
 from openpyxl.chart.label import DataLabelList
+
+import re
+import os
+import sys
+
+def get_executable_path():
+    return os.path.dirname(os.path.abspath(sys.argv[0]))
+
+
+def extract_decisions_and_cases(title):
+    """
+    Extracts decision and case information from a given title string.
+
+    This function searches for patterns that match legal decisions and cases,
+    formatted as 'CAS/TAS YYYY XXX NNN', where 'CAS' or 'TAS' are decision
+    prefixes, 'YYYY' is a year, 'XXX' is a series of letters, and 'NNN' is a number.
+    The function handles cases where multiple decisions are concatenated with '+'.
+
+    Args:
+    title (str): The title string from which to extract decisions and cases.
+
+    Returns:
+    list: A list of extracted decisions and cases, formatted as strings.
+    """
+
+    # Regex to find all instances of decisions and cases within the title
+    decisions = re.findall(r'\b(CAS|TAS)\s(\d{4}\s[A-Z]{1,3}\s\d{1,9}(?:\s?\+\s?\d{1,9})*)', title)
+    all_cases = []
+
+    for parts in decisions:
+        prefix = parts[0]  # 'CAS' or 'TAS'
+
+        # Normalize and split the case sequences
+        case_sequence = re.sub(r'\s?\+\s?', ' + ', parts[1]).split(' + ')
+        previous_year = 0000
+        previous_letters = 'X'
+
+        for case in case_sequence:
+            part = case.split()
+
+            # Handling full case format
+            if len(part) == 3:
+                year, letters, number = part
+                previous_year = year
+                previous_letters = letters
+            # Handling case format with only number
+            elif len(part) == 1:
+                year, letters, number = previous_year, previous_letters, part[0]
+
+            # Construct the full case string
+            all_cases.append(f"{prefix} {year} {letters} {number}")
+
+    return all_cases
 
 def switch_columns(worksheet, col1_index, col2_index):
     """
@@ -222,7 +279,6 @@ def plot_frequency_bar_chart(workbook, worksheet_name, data, chart_title, chart_
 
     # Write the data to the worksheet
     for i, (category, frequency) in enumerate(data, start=1):
-        print()
         worksheet.cell(row=start_row + i, column=start_col, value=category)
         worksheet.cell(row=start_row + i, column=start_col + 1, value=frequency)
 
@@ -247,3 +303,118 @@ def plot_frequency_bar_chart(workbook, worksheet_name, data, chart_title, chart_
 
     # Place the chart on the worksheet at the specified location
     worksheet.add_chart(chart, chart_location)
+
+
+def main():
+    print("Current Working Directory:", get_executable_path())
+
+    input_file_path = input("Please enter the name of the excel file (without extension): ")
+    output_file_path = input("Please enter the name of the saved excel file (without extension): ")
+
+    input_file_path += '.xlsx'
+    output_file_path += '.xlsx'
+
+    try:
+        df = pd.read_excel(get_executable_path() + '/' + input_file_path)
+    except Exception as e:
+        print(f"An error occurred while reading the Excel file: {e}")
+        return
+
+    df = df.iloc[:, [0, 2]]  # Selecting specific columns
+    df.iloc[:, 1] = pd.to_datetime(df.iloc[:, 1]).dt.date  # Convert to date-only format
+
+    decision_case_counts = defaultdict(int)
+    decisions = []
+
+    for _, row in df.iterrows():
+        decision_name = row.iloc[0]
+        decision_date = row.iloc[1]
+        cases = extract_decisions_and_cases(decision_name)
+        cases_string = ' & '.join(cases)
+        decisions.append([None, decision_name, decision_date, cases_string])
+        decision_case_counts[(cases_string, decision_date)] += 1
+
+    all_decisions_df = pd.DataFrame(decisions, columns=[None, 'Filename', 'Decision_Date', 'Cases_Numbers'])
+    all_decisions_df = all_decisions_df.sort_values(by='Decision_Date', ascending=False)
+
+    output_path = get_executable_path() + '/' + output_file_path
+    with pd.ExcelWriter(output_path, engine='openpyxl') as writer:
+        all_decisions_df.to_excel(writer, index=False, sheet_name='Cases_Numbers')
+
+        workbook = writer.book
+        worksheet = writer.sheets['Cases_Numbers']
+
+        # Apply formatting, coloring, and border to decisions
+        row_idx = 2  # Start from row 2 due to header
+        counter = 1
+        unique_years = []
+        while row_idx <= len(all_decisions_df) + 1:
+            row = all_decisions_df.iloc[row_idx - 2]
+            decision_count = decision_case_counts[(row['Cases_Numbers'], row['Decision_Date'])]
+            unique_years.append(row['Decision_Date'].year)
+            end_row = row_idx + decision_count - 1
+
+            cell_range = f'B{row_idx}:D{end_row}'
+            cell_range_counter = f'A{row_idx}:A{end_row}'
+            remove_inner_borders(worksheet, cell_range)
+            if end_row != row_idx:
+                fill_cells(worksheet, cell_range, 'solid', '00FF00')
+                set_outer_border(worksheet, cell_range, 'thick')
+
+            write_in_cells(worksheet, cell_range_counter, str(counter))
+            set_outer_border(worksheet, cell_range_counter, 'thick')
+
+            row_idx = end_row + 1
+            counter += 1
+
+        cell_alignment(worksheet,'A')
+        cell_alignment(worksheet, 'C')
+        cell_alignment(worksheet, 'D')
+
+        auto_adjust_column_width(worksheet)
+        set_outer_border(worksheet, f'B2:B{row_idx-1}', 'thin')
+        set_outer_border(worksheet, f'C2:C{row_idx-1}', 'thin')
+        set_outer_border(worksheet, None, 'thick')
+        set_outer_border(worksheet, f'A1:D1', 'thick')
+        year_counts = Counter(unique_years)
+        data_for_chart = list(year_counts.items())
+        plot_frequency_bar_chart(workbook, 'Cases_Numbers', data_for_chart, 'Decision Date Frequency', 'F1')
+
+        # Define subsets
+        subset_1 = all_decisions_df[all_decisions_df['Filename'].str.contains('CAS Web Archives')]
+        subset_2 = all_decisions_df[all_decisions_df['Filename'].str.contains('CAS Bull')]
+        subset_3 = all_decisions_df[~all_decisions_df['Filename'].isin(subset_1['Filename']) & ~all_decisions_df['Filename'].isin(subset_2['Filename'])]
+
+        # Write subsets to different sheets
+        subsets = [subset_1,subset_2,subset_3]
+        sheet_names = ['CAS Web Archives','CAS Bull','Other']
+        for i, subset in enumerate(subsets, start=1):
+            sheet_name = sheet_names[i-1]
+            workbook.create_sheet(sheet_name)
+            subset.to_excel(writer, index=False, sheet_name=sheet_name)
+
+            subset_worksheet = writer.sheets[sheet_name]
+            # Apply formatting and add counter to the first column
+            row_idx = 2  # Start from the second row
+            for _, row in subset.iterrows():
+                write_in_cells(subset_worksheet, f'A{row_idx}', str(row_idx - 1))
+                row_idx += 1
+            cell_alignment(subset_worksheet, 'A')
+            cell_alignment(subset_worksheet, 'C')
+            cell_alignment(subset_worksheet, 'D')
+            auto_adjust_column_width(subset_worksheet)
+            set_outer_border(subset_worksheet, f'B2:B{row_idx - 1}', 'thin')
+            set_outer_border(subset_worksheet, f'C2:C{row_idx - 1}', 'thin')
+            set_outer_border(subset_worksheet, f'A1:D1', 'thick')
+            set_outer_border(subset_worksheet, None, 'thick')
+
+            # Plot frequency bar chart of decision dates
+            if 'Decision_Date' in subset:
+                # Extract year from each datetime object
+                decision_years = [date.year for date in subset['Decision_Date'] if pd.notnull(date)]
+                year_counts = Counter(decision_years)
+                data_for_chart = list(year_counts.items())
+                plot_frequency_bar_chart(workbook, sheet_name, data_for_chart, 'Decision Date Frequency','F1')
+
+if __name__ == "__main__":
+    main()
